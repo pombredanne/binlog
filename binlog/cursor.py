e81366530084e3ddc0b4cf47c59b332523bef548
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2017-01-20 18:13:37 +0100

    Refactor

--- binlog/cursor.py
+++ binlog/cursor.py
@@ -2,43 +2,56 @@ from collections import namedtuple
 
 
 class CursorProxy(namedtuple('_CursorProxy', ['db', 'cursor'])):
+
+    def _to_key(self, data):
+        return self.db.K.db_value(data)
+
+    def _from_key(self, data):
+        return self.db.K.python_value(data)
+
+    def _to_value(self, data):
+        return self.db.V.db_value(data)
+
+    def _from_value(self, data):
+        return self.db.V.python_value(data)
+
     def get(self, key, default=None):
-        raw = self.cursor.get(self.db.K.db_value(key), default=None)
+        raw = self.cursor.get(self._to_key(key), default=None)
         if raw is None:
             return default
         else:
-            return self.db.V.python_value(raw)
+            return self._from_value(raw)
 
     def put(self, key, value, **kwargs):
-        return self.cursor.put(self.db.K.db_value(key),
-                               self.db.V.db_value(value),
+        return self.cursor.put(self._to_key(key),
+                               self._to_value(value),
                                **kwargs)
 
     def putmulti(self, items, **kwargs):
         def _translate_items():
             for key, value in items:
-                yield (self.db.K.db_value(key), self.db.V.db_value(value))
+                yield (self._to_key(key), self._to_value(value))
 
         return self.cursor.putmulti(_translate_items(), **kwargs)
 
     def pop(self, key):
-        res = self.cursor.pop(self.db.K.db_value(key))
+        res = self.cursor.pop(self._to_key(key))
         if res is None:
             return None
         else:
-            return self.db.V.python_value(res)
+            return self._from_value(res)
 
     def _iterate(self, iterator, keys, values):
         if keys and values:
             for raw_key, raw_value in iterator:
-                yield (self.db.K.python_value(raw_key),
-                       self.db.V.python_value(raw_value))
+                yield (self._from_key(raw_key),
+                       self._from_value(raw_value))
         elif keys:
             for raw_key in iterator:
-                yield self.db.K.python_value(raw_key)
+                yield self._from_key(raw_key)
         elif values:
             for raw_key in iterator:
-                yield self.db.V.python_value(raw_value)
+                yield self._from_value(raw_value)
         else:
             raise ValueError("keys and/or values must be true")
 
