f3d41613d0cade9d9de82da6582d926903a0f8cd
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2017-09-04 16:40:46 +0200

    Enforce same PID when using IO methods.

--- tests/unit/test_20_connection.py
+++ tests/unit/test_20_connection.py
@@ -1,12 +1,17 @@
 from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 from unittest.mock import patch
 import pickle
+import time
+import os
 
 import pytest
 
 from binlog.model import Model
 from binlog.exceptions import BadUsageError
 
+io_methods = ["data", "readers", "create", "bulk_create", "reader",
+              "register_reader", "unregister_reader", "save_registry", "list_readers",
+              "remove", "purge"]
 
 def test_model_open_returns_connection(tmpdir):
     from binlog.connection import Connection
@@ -51,17 +56,7 @@ def test_connection_is_context_manager(tmpdir):
     assert conn.closed
 
 
-@pytest.mark.parametrize("io_method", ["data",
-                                       "readers",
-                                       "create",
-                                       "bulk_create",
-                                       "reader",
-                                       "register_reader",
-                                       "unregister_reader",
-                                       "save_registry",
-                                       "list_readers",
-                                       "remove",
-                                       "purge"])
+@pytest.mark.parametrize("io_method", io_methods)
 def test_cannot_use_same_connection_from_multiple_threads(tmpdir, io_method):
     conn = Model.open(tmpdir)
 
@@ -80,17 +75,7 @@ def test_connection_cannot_be_picklelized(tmpdir):
 
 
 @pytest.mark.skip(reason="http://bugs.python.org/issue30549#")
-@pytest.mark.parametrize("io_method", ["data",
-                                       "readers",
-                                       "create",
-                                       "bulk_create",
-                                       "reader",
-                                       "register_reader",
-                                       "unregister_reader",
-                                       "save_registry",
-                                       "list_readers",
-                                       "remove",
-                                       "purge"])
+@pytest.mark.parametrize("io_method", io_methods)
 def test_cannot_use_same_connection_from_multiple_processes(tmpdir, io_method):
     conn = Model.open(tmpdir)
 
@@ -100,17 +85,7 @@ def test_cannot_use_same_connection_from_multiple_processes(tmpdir, io_method):
 
 
 
-@pytest.mark.parametrize("io_method", ["data",
-                                       "readers",
-                                       "create",
-                                       "bulk_create",
-                                       "reader",
-                                       "register_reader",
-                                       "unregister_reader",
-                                       "save_registry",
-                                       "list_readers",
-                                       "remove",
-                                       "purge"])
+@pytest.mark.parametrize("io_method", io_methods)
 def test_cannot_use_same_connection_after_when_is_closed(tmpdir, io_method):
     with Model.open(tmpdir) as db:
         pass
@@ -136,3 +111,44 @@ def test_cant_reopen_a_closed_connection(tmpdir):
     assert conn1.closed
     with pytest.raises(BadUsageError):
         conn1.open()
+
+
+@pytest.mark.parametrize("io_method", io_methods)
+def test_connection_not_allowed_from_different_processes(tmpdir, io_method):
+    r, w = os.pipe()
+    os.set_inheritable(r, True)
+    os.set_inheritable(w, True)
+
+    with Model.open(tmpdir) as db:
+        pid = os.fork()
+        if pid != 0:  # Parent
+            os.close(w)
+            with os.fdopen(r, 'rb') as pipe:
+                try:
+                    getattr(db, io_method)()  # This should not raise BadUsageError
+                except BadUsageError as exc:
+                    assert False, "Unexpected exception in parent %r" % exc 
+                except Exception as exc:
+                    # Possible expected behavior because we are not calling
+                    # every io_method with all the neded parameters
+                    pass
+                finally:
+                    # Receive child result
+                    result, message = pickle.load(pipe)
+                    assert result, message
+                    os.waitpid(pid, 0)
+        else:  # Child
+            os.close(r)
+            with os.fdopen(w, 'wb') as pipe:
+                try: 
+                    getattr(db, io_method)()  # This should not raise BadUsageError
+                except BadUsageError:
+                    pickle.dump((True, "Expected behavior"), pipe)
+                except Exception as exc:
+                    # This is not expected even when we are calling the
+                    # io_method with incorrect parameters because BadUsageError
+                    # MUST raise first given that is in the decorator.
+                    pickle.dump((False, "Unexpected exception in child %r" % exc), pipe)
+                else:
+                    pickle.dump((False, "Child did not raise"), pipe)
+            os._exit(0)
